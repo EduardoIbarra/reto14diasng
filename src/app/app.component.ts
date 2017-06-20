0971abbc4c4c06cff530dbbe7404eb957afc117f
Eduardo <ibredu@gmail.com>
2017-06-19 19:27:41 -0500

    Added simple example of ng-for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  my_notes = [
+    {id:1, title: 'Note 1', description: 'Description for note 1'},
+    {id:2, title: 'Note 2', description: 'Description for note 2'},
+    {id:3, title: 'Note 3', description: 'Description for note 3'},
+    {id:4, title: 'Note 4', description: 'Description for note 4'},
+  ];
 }
